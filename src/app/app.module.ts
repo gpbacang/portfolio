efdfdf40858e13a2d93f8ffa6939d2c8c3a49ded
gpbacang <gpbacang@Geraldines-MacBook-Pro.local>
2017-08-13 17:50:40 +0800

    Add home and side menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
